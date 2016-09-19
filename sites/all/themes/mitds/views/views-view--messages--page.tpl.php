d03d8627fb68b70b7646c9bbf8aef0372bda1970
Salu S Nair <salu.s@lightrains.com>
2016-09-19 18:12:06 +0530

    Designing Notification page

---
+++ sites/all/themes/mitds/views/views-view--messages--page.tpl.php
@@ -0,0 +1,40 @@
+<div class="<?php print $classes; ?>">
+
+	<?php if ($header): ?>
+	<div class="view-header">
+		<?php print $header; ?>
+	</div>
+<?php endif; ?>
+
+<ul class="notifi">
+	<?php foreach ($view->style_plugin->rendered_fields as $key => $value) { 
+		print_r($value);
+		$msg = '';
+		switch ($value['type']) {
+			case 'notifyauthorproposal':
+				$msg = 'Proposal Submitted';
+				break;
+			case 'commentslog':
+				$msg = 'Comment Received';
+				break;
+			default:
+				# code...
+				break;
+		}
+	?>
+
+	<li class="<?php print $value['type']; ?>">
+		<strong><?php print $msg; ?></strong>
+		<p><?php print $value['message_render']; ?></p>
+		<span><?php print $value['timestamp']; ?></span>
+		<span><?php print $value['uid']; ?></span>
+	</li>
+
+	<?php } ?>
+</ul>
+
+<?php if ($pager): ?>
+	<?php print $pager; ?>
+<?php endif; ?>
+
+</div>
