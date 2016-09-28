caaccc2f39a9900bac9e3f684e0103515ebdc07d
Salu S Nair <salu.s@lightrains.com>
2016-09-28 16:42:20 +0530

    Phone number module

---
+++ sites/all/modules/light/light_misc/light_misc.js
@@ -0,0 +1,17 @@
+// (function ($) {
+
+//   Drupal.behaviors.international_phone = {
+//     attach: function (context, settings) {
+//       $(".international_phone-number").intlTelInput({
+//       	onlyCountries: ["al", "ad", "at", "by", "be", "ba", "bg", "hr", "cz", "dk", "ee", "fo", "fi", "fr", "de", "gi", "gr", "va", "hu", "is", "ie", "it", "lv", "li", "lt", "lu", "mk", "mt", "md", "mc", "me", "nl", "no", "pl", "pt", "ro", "ru", "sm", "rs", "sk", "si", "es", "se", "ch", "ua", "us", "gb", "in"],
+//       	preferredCountries: ["us", "in", "gb"],
+//       	initialCountry: "in",
+//       	separateDialCode: true,
+//       	autoFormat: false
+//         //utilsScript: "lib/libphonenumber/build/utils.js"
+//       });
+
+//     }
+//   };
+
+// }(jQuery));
