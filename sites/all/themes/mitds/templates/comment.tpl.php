84daa290901cb98b0938ed3fa4d6c97a6a68fedf
Salu S Nair <salu.s@lightrains.com>
2016-10-04 18:04:03 +0530

    Changed comment form design

--- sites/all/themes/mitds/templates/comment.tpl.php
+++ sites/all/themes/mitds/templates/comment.tpl.php
@@ -8,10 +8,10 @@ $name = $node->name;
 <div class="message-data <?php echo ($name == $comment->name) ? ' align-right' : ''; ?>">
   
   <?php if($name == $comment->name) { ?> 
-  <span class="message-data-time"><?php echo $created; ?></span> &nbsp; &nbsp;
+  <?php echo $permalink; ?><span class="message-data-time"><?php echo $created; ?></span> &nbsp; &nbsp;
   <span class="message-data-name"><?php echo $author; ?></span> <i class="fa fa-circle <?php echo ($name == $comment->name) ? ' me' : ' online'; ?>"></i>
   <?php } else { ?>
-  <i class="fa fa-circle <?php echo ($name == $comment->name) ? ' me' : ' online'; ?>"></i><span class="message-data-name"><?php echo $author; ?></span>&nbsp; &nbsp; 
+  <?php echo $permalink; ?><i class="fa fa-circle <?php echo ($name == $comment->name) ? ' me' : ' online'; ?>"></i><span class="message-data-name"><?php echo $author; ?></span>&nbsp; &nbsp; 
   <span class="message-data-time"><?php echo $created; ?></span>
 
   <?php } ?>
@@ -19,8 +19,33 @@ $name = $node->name;
   
 </div>
 <div class="message <?php echo ($name == $comment->name) ? ' other-message float-right' : ' my-message'; ?>">
-  <?php echo $comment->comment_body['und'][0]['safe_value']; ?>
+
+
+  <?php echo nl2br($comment->comment_body['und'][0]['safe_value']); ?>
+
+  
+
+  <?php if(isset($comment->field_proposal_files['und'])) { ?>
+  
+  <ol class="commentFiles mar-top10">
+  <?php 
+  $commentFiles = $comment->field_proposal_files['und'];
+  foreach ($commentFiles as $key => $value) { 
+    // $imgpath = file_load($value['uri']);
+    $fpath = file_create_url($value['uri']);
+?> 
+
+    <li><a href="<?php echo $fpath; ?>"><?php echo $value['filename']; ?></a></li>
+  
+  <?php } ?>
+  </ol>  
+
+  <?php } ?>
+
+
 </div>
+
+
 
 </li>
 
