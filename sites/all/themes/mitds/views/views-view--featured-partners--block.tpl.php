be6679e9e7c628ab9ac5bf26ddcbdc062f5fa3b3
Salu S Nair <salu.s@lightrains.com>
2016-12-13 17:39:04 +0530

    Featured partners block in home page

---
+++ sites/all/themes/mitds/views/views-view--featured-partners--block.tpl.php
@@ -0,0 +1,19 @@
+<section class="pro-mem">
+    <div class="container pb30">      
+      <div class="row"> 
+
+      <?php foreach ($view->style_plugin->rendered_fields as $key => $value) {  //print_r($value);exit;
+          $name = $value['field_first_name'].' '.$value['field_last_name'];
+        ?>                                               
+        <div class="col-sm-3">
+          <div class="uou-block-6a"> 
+            <?php print $value['field_company_logo']; ?>  
+            <h6><?php print $name; ?> <span><i class="fa fa-map-marker"></i> <?php print $value['country']; ?>, <?php print $value['province']; ?></span></h6>
+           
+          </div>
+          <!-- end .uou-block-6a --> 
+        </div>
+        <?php } ?>
+      </div>
+    </div>
+  </section>
