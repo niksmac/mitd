d03ba074cc5319e1f8c071eef027bd08fef7c33f
Salu S Nair <salu.s@lightrains.com>
2016-12-23 17:53:22 +0530

    Span added for organization name section

--- sites/all/themes/mitds/views/views-view--partners-list--page-1.tpl.php
+++ sites/all/themes/mitds/views/views-view--partners-list--page-1.tpl.php
@@ -23,11 +23,11 @@
 
       <div class="tag-cloud">
         <?php if($value['country']!='') { ?>
-        <span class="btn btn-primary"><?php print $value['country']; ?><?php } ?></span>
+        <span class="label label-default"><?php print $value['country']; ?><?php } ?></span>
         <?php if($value['field_organization_name']!='') { ?>
-        <span class="btn btn-primary"><?php print $value['field_organization_name']; ?><?php } ?></span>
+        <span class="label label-primary org-name"><?php print $value['field_organization_name']; ?><?php } ?></span>
         <?php if($value['field_organization_url']!='') { ?>
-        <span class="btn btn-primary"><?php print $value['field_organization_url']; ?><?php } ?></span>
+        <span class="label label-default"><?php print $value['field_organization_url']; ?><?php } ?></span>
       </div>
 
     </div>
