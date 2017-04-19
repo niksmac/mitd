4c278450d9da95531b5f0559126df11c37aa7ec4
Salu S Nair <salu.s@lightrains.com>
2017-04-19 11:18:49 +0530

    Management page changes

--- sites/all/themes/mitds/views/views-view--management--page.tpl.php
+++ sites/all/themes/mitds/views/views-view--management--page.tpl.php
@@ -1,26 +1,18 @@
-<p>Appropriately architect error-free opportunities for sticky results. Holisticly myocardinate cross-media data via market positioning markets. Objectively maximize orthogonal technology through customer directed services. Intrinsicly build error-free initiatives through.Interactively optimize diverse infomediaries rather than viral alignments. Collaboratively actualize bleeding-edge methods of empowerment for next-generation metrics. Completely enhance multimedia based human capital vis-a-vis mission-critical communities. Dynamically supply low-risk high-yield markets via emerging catalysts for change. Collaboratively cultivate multimedia based innovation via innovative applications.
-
-Authoritatively unleash distributed channels whereas empowered core competencies. Interactively network virtual leadership skills before fully tested ideas. Conveniently procrastinate empowered deliverables through extensible methods of empowerment. Competently negotiate cross-platform sources with prospective e-tailers. Efficiently engineer effective architectures rather than viral collaboration and idea-sharing.</p>
-
-<?php if($rows): ?>
+<?php if ($rows): ?>
 <div class="row testimonials text-left margin-bottom-md-2 management-team">
 
-<?php foreach ($view->style_plugin->rendered_fields as $key => $value) {  //print_r($value); ?>
+<?php foreach ($view->style_plugin->rendered_fields as $key => $value) { //print_r($value); ?>
 
     <div class="col-md-4 text-center col-sm-6 col-xs-12  team">
-      <div class="staff" data-toggle="tooltip" data-placement="bottom" title="<?php echo (isset($value['body'])) ? $value['body'] : ""; ?>">
+      <div class="staff1" data-toggle="tooltip" data-placement="bottom">
         <?php echo $value['field_manage_image']; ?>
         <h3 class="staff-name"><?php echo $value['title']; ?></h3>
-        <p class=""><?php echo $value['field_manage_designation']; ?></p>
+        <p class=""><b>[ <?php echo $value['field_manage_designation']; ?> ]</b></p>
+        <?php echo $value['body']; ?>
       </div>
     </div>
 
-<?php } ?>
+<?php }?>
 </div>
-<?php endif; ?>
+<?php endif;?>
 
-<p><b>Synergistically enable cross-unit web-readiness with real-time services. Professionally repurpose cost effective experiences rather than sustainable action items.</b></p>
-
-<p> Appropriately reinvent process-centric deliverables before visionary methodologies. Continually re-engineer economically sound markets before just in time products. Intrinsicly exploit parallel opportunities whereas plug-and-play schemas.
-
-Phosfluorescently strategize front-end niches without seamless architectures. Phosfluorescently reconceptualize enabled leadership for superior "outside the box" thinking. Energistically communicate client-based intellectual capital with plug-and-play convergence. Progressively conceptualize go forward paradigms whereas professional applications. Energistically communicate functionalized opportunities vis-a-vis.</p>
