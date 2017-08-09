4e2217b2dfc6d24eed4d006e99b289dac48155ff
Salu S Nair <salu.s@lightrains.com>
2017-08-09 09:28:42 +0530

    No changes

--- sites/all/themes/mitds/templates/node--proposals.tpl.php
+++ sites/all/themes/mitds/templates/node--proposals.tpl.php
@@ -1,29 +1,29 @@
-<?php if($view_mode == "full") : ?>
+<?php if ($view_mode == "full"): ?>
 <div class="profile-company-content">
 <div class="row">
-  <?php 
- // print_r($node);
-  ?>
-  <div class="col-md-8"> 
+  <?php
+// print_r($node);
+?>
+  <div class="col-md-8">
     <div class="sidebar">
       <h5 class="main-title">Proposal Description</h5>
-      <div class="sidebar-information"> 
+      <div class="sidebar-information">
         <div class="single-category">
-          <p><?php print render($content['field_proposal_description']); ?></p>
+          <p><?php print render($content['field_proposal_description']);?></p>
           <h5 class="main-title">Milestones</h5>
-          <p><?php print render($content['field_milestones']); ?></p>
+          <p><?php print render($content['field_milestones']);?></p>
           <h5 class="main-title">Key Deliverables</h5>
-          <p><?php print render($content['field_key_deliverables']); ?></p>
+          <p><?php print render($content['field_key_deliverables']);?></p>
           <h5 class="main-title">Remarks</h5>
-          <p><?php print render($content['field_remarks']); ?></p>
+          <p><?php print render($content['field_remarks']);?></p>
         </div>
       </div>
     </div>
 
-    <?php print render($content['comments']); ?> 
-                    
+    <?php print render($content['comments']);?>
+
   </div>
-  <div class="col-md-4"> 
+  <div class="col-md-4">
 <!-- Professional Details -->
     <div class="sidebar">
       <h5 class="main-title">Proposal Details</h5>
@@ -31,15 +31,15 @@
         <ul class="single-category">
           <li class="row">
             <h6 class="title col-xs-6">Opportunity</h6>
-            <span class="subtitle col-xs-6"><?php print render($content['field_opportunity']); ?></span>
+            <span class="subtitle col-xs-6"><?php print render($content['field_opportunity']);?></span>
           </li>
           <li class="row">
             <h6 class="title col-xs-6">Project Duration(Weeks)</h6>
-            <span class="subtitle col-xs-6"><?php print render($content['field_project_duration_weeks_']); ?></span>
+            <span class="subtitle col-xs-6"><?php print render($content['field_project_duration_weeks_']);?></span>
           </li>
           <li class="row">
             <h6 class="title col-xs-6">Pricing</h6>
-            <span class="subtitle col-xs-6"><?php print render($content['field_pricing']); ?></span>
+            <span class="subtitle col-xs-6"><?php print render($content['field_pricing']);?></span>
           </li>
           <!-- <li class="row">
             <h6 class="title col-xs-6">Milestones</h6>
@@ -47,32 +47,33 @@
           </li> -->
           <li class="row">
             <h6 class="title col-xs-6">Cost(USD)</h6>
-            <span class="subtitle col-xs-6"><?php print render($content['field_cost_usd_fixed']); ?></span>
+            <span class="subtitle col-xs-6"><?php print render($content['field_cost_usd_fixed']);?></span>
           </li>
-        </ul>        
+        </ul>
       </div>
-    </div> 
-    <?php if($node->uid != $user->uid && !in_array("admin", $user->roles)) {  ?>
-    <?php if($node->field_short_list['und']['0']['value'] == 1) { ?>
-      <a href="<?php echo url("unshortlist/".$nid, array('query' => array('destination' => $_GET['q'])))?>" class="btn btn-default btn-block">Short Listed</a> 
-    <?php } else { ?>
-    <a href="<?php echo url("proposal/".$nid, array('query' => array('destination' => $_GET['q'])))?>" class="btn btn-primary btn-block">Short List</a>
-    <?php } ?>
-    <?php 
-    $op_id = $node->field_opportunity['und']['0']['nid'];
-    $awarded = _check_awarded_opportunity($op_id);    
-    if($awarded == 1) {
-    ?>
-    
-    <a href="#" class="btn btn-default btn-block">This opportunity is awarded</a> 
-    <?php } else { ?>
-  
-    <a href="<?php echo url("proposal/award/".$nid, array('query' => array('destination' => $_GET['q'])))?>" class="btn btn-primary btn-block">Award Proposal</a>
-    <?php } }?>
-   
-  </div>  
+    </div>
+    <?php if ($node->uid != $user->uid && !in_array("admin", $user->roles)) {
+	?>
+    <?php if ($node->field_short_list['und']['0']['value'] == 1) {?>
+      <a href="<?php echo url("unshortlist/" . $nid, array('query' => array('destination' => $_GET['q']))) ?>" class="btn btn-default btn-block">Short Listed</a>
+    <?php } else {?>
+    <a href="<?php echo url("proposal/" . $nid, array('query' => array('destination' => $_GET['q']))) ?>" class="btn btn-primary btn-block">Short List</a>
+    <?php }?>
+    <?php
+$op_id = $node->field_opportunity['und']['0']['nid'];
+	$awarded = _check_awarded_opportunity($op_id);
+	if ($awarded == 1) {
+		?>
+
+    <a href="#" class="btn btn-default btn-block">This opportunity is awarded</a>
+    <?php } else {?>
+
+    <a href="<?php echo url("proposal/award/" . $nid, array('query' => array('destination' => $_GET['q']))) ?>" class="btn btn-primary btn-block">Award Proposal</a>
+    <?php }}?>
+
+  </div>
 </div>
 </div>
 </div>
 
-<?php endif; ?>
+<?php endif;?>
