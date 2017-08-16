9750d3aac85aa54e35375778f69d83518d734605
Salu S Nair <salu.s@lightrains.com>
2017-08-16 11:09:28 +0530

    Changed title

--- sites/all/themes/mitds/views/views-view--proposals--block.tpl.php
+++ sites/all/themes/mitds/views/views-view--proposals--block.tpl.php
@@ -2,38 +2,38 @@
 <div class="air-card mar-bot50">
 <div class="similar">
 
-  <?php 
-  if ( empty($title) ): 
-    $title = $view->get_title(); 
-    endif; 
-    if ($title): ?>
+  <?php
+if (empty($title)):
+	$title = $view->get_title();
+endif;
+if ($title): ?>
       <h3 class="sub-title"><b><?php print t($title)?></b></h3>
-    <?php endif; ?>
-    <?php foreach ($view->style_plugin->rendered_fields as $key => $value) {//print_r($user->uid);exit;?>
-    <div class="media">                           
+    <?php endif;?>
+    <?php foreach ($view->style_plugin->rendered_fields as $key => $value) { //print_r($user->uid);exit;?>
+    <div class="media">
         <div class="media-body list-title">
-            <h5><?php echo $value['title']; ?></h5>            
+            <h5><?php echo $value['title']; ?></h5>
             <p><?php echo $value['field_proposal_description']; ?></p>
-            <?php if(!empty($value['field_first_name']) ||  !empty($value['field_last_name'])) { ?>
-            <p class="text-success"><?php echo $value['field_first_name'].' '.$value['field_last_name']; ?></p><?php } ?>           
+            <?php if (!empty($value['field_first_name']) || !empty($value['field_last_name'])) {?>
+            <p class="text-success"><?php echo $value['field_first_name'] . ' ' . $value['field_last_name']; ?></p><?php }?>
             <div class="share-w">
-                <a href="<?php echo url("node/".$value['nid']); ?>" data-toggle="tooltip" data-placement="top" title="View">
+                <a href="<?php echo url("node/" . $value['nid']); ?>" data-toggle="tooltip" data-placement="top" title="View">
                     <i class="fa fa-bookmark-o"></i>
                 </a>
-                
-                <?php if(!(isset($value['uid']))) { ?>
-                <?php if(isset($value['field_short_list']) && $value['field_short_list'] == 1) { ?>
-                <a href="<?php echo url("unshortlist/".$value['nid'], array('query' => array('destination' => $_GET['q'])))?>" data-toggle="tooltip" data-placement="top" title="Shortlisted">
+
+                <?php if (!(isset($value['uid']))) {?>
+                <?php if (isset($value['field_short_list']) && $value['field_short_list'] == 1) {?>
+                <a href="<?php echo url("unshortlist/" . $value['nid'], array('query' => array('destination' => $_GET['q']))) ?>" data-toggle="tooltip" data-placement="top" title="Shortlisted">
                     <i class="fa fa-star" aria-hidden="true"></i>
-                </a> 
-                <?php } else { ?>
-                <a href="<?php echo url("proposal/".$value['nid'], array('query' => array('destination' => $_GET['q'])))?>" data-toggle="tooltip" data-placement="top" title="Shortlist">
+                </a>
+                <?php } else {?>
+                <a href="<?php echo url("proposal/" . $value['nid'], array('query' => array('destination' => $_GET['q']))) ?>" data-toggle="tooltip" data-placement="top" title="Shortlist">
                     <i class="fa fa-star-o" aria-hidden="true"></i>
                 </a>
-                <?php } }?>                             
+                <?php }}?>
             </div>
 
-        </div>        
+        </div>
     </div>
     <?php }?>
 </div>
@@ -41,7 +41,7 @@
 <?php elseif ($empty): ?>
     <div class="air-card mar-bot50">
     <div class="view-empty">
-      <?php print $empty; ?>
+      <?php print $empty;?>
     </div>
 </div>
-<?php endif;?>                                                                             
+<?php endif;?>
