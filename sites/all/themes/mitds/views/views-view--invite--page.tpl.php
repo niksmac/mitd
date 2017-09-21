38a0825e0ae4d6426844a6834d64c13635280f95
Salu S Nair <salu.s@lightrains.com>
2017-09-21 14:00:38 +0530

    Fixed issue with images

--- sites/all/themes/mitds/views/views-view--invite--page.tpl.php
+++ sites/all/themes/mitds/views/views-view--invite--page.tpl.php
@@ -1,67 +1,68 @@
 <?php if ($rows): ?>
-<div class=" <?php print $classes; ?>">
+<div class=" <?php print $classes;?>">
 
-  <div class="row">  
+  <div class="row">
     <div class="col-md-12">
       <?php if ($exposed): ?>
       <div class="view-filters">
-      <?php print $exposed; ?>
+      <?php print $exposed;?>
       </div>
-      <?php endif; ?>
-    </div>    
+      <?php endif;?>
+    </div>
   </div>
 
-  <?php foreach ($view->style_plugin->rendered_fields as $key => $value) { 
-    $status = _check_awarded_opportunity(arg(1)); 
-    $is_invited = _check_opportunity_invited(arg(1), $value['uid']);           
-    $name = $value['field_first_name'].' '.$value['field_last_name'];
-    ?>
+  <?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
+	//print_r($value);exit;
+	$status = _check_awarded_opportunity(arg(1));
+	$is_invited = _check_opportunity_invited(arg(1), $value['uid']);
+	$name = $value['field_first_name'] . ' ' . $value['field_last_name'];
+	?>
 
   <div class="col-md-12 air-card mar-bot10">
     <div class="col-md-2">
-      <?php print $value['picture']; ?>
-      <?php if($is_invited == 1) { ?>
+      <?php print $value['field_picture'];?>
+      <?php if ($is_invited == 1) {?>
         <a href="javascript:void();" class="btn btn-success btn-block btn-small invited">Invited</a>
-      <?php } ?>
-      <?php if($status == false && $is_invited == 0) { ?>
-        <a href="<?php echo url("invite/".arg(1)."/".$value['uid'],array('query' => array('destination' => $_GET['q'])))?>" class="btn btn-warning btn-block btn-small">Invite</a>
-      <?php } ?>
+      <?php }?>
+      <?php if ($status == false && $is_invited == 0) {?>
+        <a href="<?php echo url("invite/" . arg(1) . "/" . $value['uid'], array('query' => array('destination' => $_GET['q']))) ?>" class="btn btn-warning btn-block btn-small">Invite</a>
+      <?php }?>
     </div>
     <div class="col-md-10">
       <h3><?php echo $name; ?></h3>
-      <?php if (isset($value['field_application_description'])) { ?>
+      <?php if (isset($value['field_application_description'])) {?>
       <?php echo $value['field_application_description']; ?>
-      <?php } ?>
+      <?php }?>
       <div class="tag-cloud">
-        <?php if($value['field_geography_of_operation']!='') { ?>
-        <span class="btn btn-primary btn-small"><i class="fa fa-map-marker" aria-hidden="true"></i> <?php print $value['field_geography_of_operation']; ?><?php } ?></span>
-        <?php if($value['field_organization_name']!='') { ?>
-        <span class="btn btn-primary btn-small"><i class="fa fa-university" aria-hidden="true"></i> <?php print $value['field_organization_name']; ?><?php } ?></span>
-        <?php if($value['field_year_of_incorporation']!='') { ?>
-        <span class="btn btn-primary btn-small"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> <?php print $value['field_year_of_incorporation']; ?><?php } ?></span>
+        <?php if ($value['field_geography_of_operation'] != '') {?>
+        <span class="btn btn-primary btn-small"><i class="fa fa-map-marker" aria-hidden="true"></i> <?php print $value['field_geography_of_operation'];?><?php }?></span>
+        <?php if ($value['field_organization_name'] != '') {?>
+        <span class="btn btn-primary btn-small"><i class="fa fa-university" aria-hidden="true"></i> <?php print $value['field_organization_name'];?><?php }?></span>
+        <?php if ($value['field_year_of_incorporation'] != '') {?>
+        <span class="btn btn-primary btn-small"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> <?php print $value['field_year_of_incorporation'];?><?php }?></span>
       </div>
     </div>
-    
+
   </div>
-  <?php } ?>
+  <?php }?>
 </div>
 
 <?php if ($pager): ?>
-  <?php print $pager; ?>
-<?php endif; ?>
+  <?php print $pager;?>
+<?php endif;?>
 
 <?php elseif ($empty): ?>
-  <div class="row">  
+  <div class="row">
     <div class="col-md-12">
       <?php if ($exposed): ?>
       <div class="view-filters">
-      <?php print $exposed; ?>
+      <?php print $exposed;?>
       </div>
-      <?php endif; ?>
-    </div>    
+      <?php endif;?>
+    </div>
   </div>
 
     <div class="col-md-12 air-card mar-bot10">
-      <?php print $empty; ?>
+      <?php print $empty;?>
     </div>
-<?php endif; ?>
+<?php endif;?>
