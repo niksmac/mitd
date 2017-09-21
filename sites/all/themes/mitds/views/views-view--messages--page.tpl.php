b5279fad58c2c570b8b10839721413be1dfff0d9
Salu S Nair <salu.s@lightrains.com>
2017-09-21 10:14:45 +0530

    Fixed spelling and spacing error

--- sites/all/themes/mitds/views/views-view--messages--page.tpl.php
+++ sites/all/themes/mitds/views/views-view--messages--page.tpl.php
@@ -1,57 +1,57 @@
 <?php if ($rows): ?>
-	<div class="<?php print $classes; ?>">
+	<div class="<?php print $classes;?>">
 
 		<?php if ($header): ?>
 		<div class="view-header">
-			<?php print $header; ?>
+			<?php print $header;?>
 		</div>
-	<?php endif; ?>
+	<?php endif;?>
 
 
 
 	<ul class="notifi">
-		<?php foreach ($view->style_plugin->rendered_fields as $key => $value) { 		
-			$msg = '';
-			switch ($value['type']) {
-				case 'notifyauthorproposal':
-				$msg = 'Proposal Submitted';
-				break;
-				case 'commentslog':
-				$msg = 'Comment Received';
-				break;
-				case 'notifyuserwhenopportunityawarded':
-				$msg = 'Opportunity Awarded';
-				break;
-				case 'notifyuserwheninvited':
-				$msg = 'You are invited';
-				break;
-				case 'notifyuserwheninvitedcollaboration':
-				$msg = 'You are invited for collaboration';
-				break;
-				case 'notifyuserswhenaddedtocollaboration':
-				$msg = 'You are added to the collaboration group';
-				break;
-				case 'notifyuserwhenproposalshortlisted':
-				$msg = 'Youre proposal hasbeen shortlisted';
-				break;
-				default:
-				# code...
-				break;
-			}
-			?>
+		<?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
+	$msg = '';
+	switch ($value['type']) {
+	case 'notifyauthorproposal':
+		$msg = 'Proposal Submitted';
+		break;
+	case 'commentslog':
+		$msg = 'Comment Received';
+		break;
+	case 'notifyuserwhenopportunityawarded':
+		$msg = 'Opportunity Awarded';
+		break;
+	case 'notifyuserwheninvited':
+		$msg = 'You are invited';
+		break;
+	case 'notifyuserwheninvitedcollaboration':
+		$msg = 'You are invited for collaboration';
+		break;
+	case 'notifyuserswhenaddedtocollaboration':
+		$msg = 'You are added to the collaboration group';
+		break;
+	case 'notifyuserwhenproposalshortlisted':
+		$msg = 'Your proposal has been shortlisted';
+		break;
+	default:
+		# code...
+		break;
+	}
+	?>
 
-			<li class="<?php print $value['type']; ?>">
-				<strong><?php print $msg; ?></strong>
-				<p><?php print $value['message_render']; ?></p>
-				<span><?php print $value['timestamp']; ?></span>		
+			<li class="<?php print $value['type'];?>">
+				<strong><?php print $msg;?></strong>
+				<p><?php print $value['message_render'];?></p>
+				<span><?php print $value['timestamp'];?></span>
 			</li>
 
-			<?php } ?>
+			<?php }?>
 		</ul>
 
 		<?php if ($pager): ?>
-		<?php print $pager; ?>
-	<?php endif; ?>
+		<?php print $pager;?>
+	<?php endif;?>
 
 </div>
 <?php //endif; ?>
@@ -59,6 +59,6 @@
 
 <?php elseif ($empty): ?>
 	<div class="view-empty">
-		<?php print $empty; ?>
+		<?php print $empty;?>
 	</div>
-<?php endif; ?>
+<?php endif;?>
