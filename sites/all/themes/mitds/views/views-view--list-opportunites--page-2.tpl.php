6b1b9608fcc6ca4717ad73ffc7ddbe770878e157
Salu S Nair <salu.s@lightrains.com>
2017-09-22 10:12:05 +0530

    Changed font size of opportunity title

--- sites/all/themes/mitds/views/views-view--list-opportunites--page-2.tpl.php
+++ sites/all/themes/mitds/views/views-view--list-opportunites--page-2.tpl.php
@@ -1,11 +1,12 @@
 <?php if ($rows): ?>
-  <div class="profile"<?php print $attributes; ?>>
-    <?php foreach ($view->style_plugin->rendered_fields as $key => $value) { ?>
+  <div class="profile"<?php print $attributes;?>>
+    <?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
+	?>
     <div class="listing listing-1 listing-new-style">
       <div class="listing-section">
         <div class="listing-ver-3">
           <div class="listing-heading">
-            <h5><?php echo $value['title']; ?></h5>          
+            <h2><?php echo $value['title']; ?></h2>
           </div>
           <div class="listing-inner">
             <div class="listing-content">
@@ -16,44 +17,44 @@
           <div class="listing-tabs">
             <ul>
               <li><a href="#"><?php echo $value['field_project_budget']; ?></a> </li>
-              <?php 
-              global $user; 
-              $sub = _check_subscription_count($user->uid);
-              $awarded = _check_awarded_opportunity($value['nid']);
-              $proposal_data = mitd_nid($user->uid, $value['nid']);
-              ?>
+              <?php
+global $user;
+	$sub = _check_subscription_count($user->uid);
+	$awarded = _check_awarded_opportunity($value['nid']);
+	$proposal_data = mitd_nid($user->uid, $value['nid']);
+	?>
               <li>
 
-                <?php if($awarded == 0) { ?>
-                <?php if(!empty($proposal_data)){  ?>
-                <a href="<?php echo url("node/".$proposal_data[0]); ?>">View Proposal</a>
-                <?php } else { ?>
+                <?php if ($awarded == 0) {?>
+                <?php if (!empty($proposal_data)) {?>
+                <a href="<?php echo url("node/" . $proposal_data[0]); ?>">View Proposal</a>
+                <?php } else {?>
 
-                <?php if(_check_subscription_count($user->uid)) { ?>
-                <a href="<?php echo url("node/add/proposals/".$value['nid'])?>">Submit Proposal</a>
-                <?php  } else { ?> 
+                <?php if (_check_subscription_count($user->uid)) {?>
+                <a href="<?php echo url("node/add/proposals/" . $value['nid']) ?>">Submit Proposal</a>
+                <?php } else {?>
                 <a href="#">No Bid Left</a>
-                <?php }  ?>
+                <?php }?>
 
-                <?php }  ?>
-                <?php } else { ?>
+                <?php }?>
+                <?php } else {?>
                 <a href="#">Awarded</a>
-                <?php } ?>
-              </li>   
+                <?php }?>
+              </li>
             </ul>
           </div>
 
         </div>
       </div>
     </div>
-    <?php } ?>
+    <?php }?>
     <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
+    <?php print $pager;?>
+  <?php endif;?>
 </div>
-</div> 
+</div>
 <?php elseif ($empty): ?>
   <div class="col-md-12 air-card mar-bot10">
-    <?php print $empty; ?>
-  </div>    
-<?php endif; ?>
+    <?php print $empty;?>
+  </div>
+<?php endif;?>
