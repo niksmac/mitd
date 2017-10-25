8eddf26c1f612f17ad5bae5ffdcfe3e595d15471
Salu S Nair <salu.s@lightrains.com>
2017-10-25 15:24:06 +0530

    Feedback form popup section changes

--- sites/all/themes/mitds/js/mitds.js
+++ sites/all/themes/mitds/js/mitds.js
@@ -1,33 +1,11 @@
 jQuery( document ).ready(function() {
-jQuery('.texti-slide').owlCarousel({
-    loop:true,
-    nav:true,
-    items: 2,
-    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-      responsive:{
-          0:{
-              items:1
-          },
-          800:{
-              items:2
-          },
 
-      1200:{
-              items:2
-          },
-  }});
-jQuery('.sponsors-slider').owlCarousel({
-    items: 6
-  });
-// jQuery('.uou-tabs').uouTabs();
-
-// Superfish Menus
-// ---------------------------------------------------------
-if (jQuery.fn.superfish) {
-  jQuery('.sf-menu').superfish();
-} else {
-  console.warn('not loaded -> superfish.min.js and hoverIntent.js');
-}
+  if (jQuery.fn.superfish) {
+    jQuery('.sf-menu').superfish();
+  } else {
+    console.warn('not loaded -> superfish.min.js and hoverIntent.js');
+  }
+});
 
 (function ($) {
   console.log("E");
@@ -63,13 +41,10 @@ jQuery("#edit-select-all").click(function(){
     jQuery('input:checkbox').not(this).prop('checked', this.checked);
 });
 
-jQuery('.colorbox-node').click(function(){
-        //setTimeout("jQuery('.colorbox-node')", 3000);
-        //alert('dd');
-    });
-setTimeout('jQuery( ".colorbox-node" ).trigger( "click" )',3000);
-
-});
-
-
-
+jQuery(document).idle({
+  onIdle: function(){
+    jQuery( ".colorbox-node" ).trigger( "click" );
+  },
+  keepTracking: false,
+  idle: 5000
+})
--- sites/all/themes/mitds/template.php
+++ sites/all/themes/mitds/template.php
@@ -5,69 +5,63 @@
  */
 
 function mitds_process_page(&$variables) {
-  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
-    $variables['page_class'] = 'col-md-6';
-  }
-
-  elseif (!empty($variables['page']['sidebar_first'])) {
-    $variables['page_class'] = 'col-md-9';
-  }
-  elseif (!empty($variables['page']['sidebar_second'])) {
-    $variables['page_class'] = 'col-md-9';
-  }
-  else {
-    $variables['page_class'] = 'col-md-12';
-  }
+	if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+		$variables['page_class'] = 'col-md-6';
+	} elseif (!empty($variables['page']['sidebar_first'])) {
+		$variables['page_class'] = 'col-md-9';
+	} elseif (!empty($variables['page']['sidebar_second'])) {
+		$variables['page_class'] = 'col-md-9';
+	} else {
+		$variables['page_class'] = 'col-md-12';
+	}
 }
-
 
 /**
  * Implements hook_preprocess_page().
  */
 function mitds_preprocess_page(&$variables) {
 
-  global $user;
-  if(isset($user->name)) {
-    $variables['user_name'] = $user->name;
-  }
+	global $user;
+	if (isset($user->name)) {
+		$variables['user_name'] = $user->name;
+	}
 
-  if (arg(0) == "user" && is_numeric(arg(1))){
-    $variables['theme_hook_suggestions'][] =  'page__dash';
+	if (arg(0) == "user" && is_numeric(arg(1))) {
+		$variables['theme_hook_suggestions'][] = 'page__dash';
 
-    if ($user->uid != arg(1)){
-      $variables['tabs'] = array();
-    }
-    $user_data = user_load(arg(1));
-    $variables['full_name'] =  !empty($user_data->field_first_name['und'][0]['value']) ? $user_data->field_first_name['und'][0]['value']." ".$user_data->field_last_name['und'][0]['value'] : "";
-    $variables['designation'] = !empty($user_data->field_designation) ? $user_data->field_designation['und'][0]['value'] : "";
-  }
+		if ($user->uid != arg(1)) {
+			$variables['tabs'] = array();
+		}
+		$user_data = user_load(arg(1));
+		$variables['full_name'] = !empty($user_data->field_first_name['und'][0]['value']) ? $user_data->field_first_name['und'][0]['value'] . " " . $user_data->field_last_name['und'][0]['value'] : "";
+		$variables['designation'] = !empty($user_data->field_designation) ? $user_data->field_designation['und'][0]['value'] : "";
+	}
 
-  if(in_array("admin", $user->roles) && ((arg(0) == "user" && is_numeric(arg(1))) || arg(0) =="madmin") ){
-    $variables['theme_hook_suggestions'][] =  'page__admin';
-  }
+	if (in_array("admin", $user->roles) && ((arg(0) == "user" && is_numeric(arg(1))) || arg(0) == "madmin")) {
+		$variables['theme_hook_suggestions'][] = 'page__admin';
+	}
+
 }
-
 
 /**
  * Implements hook_preprocess_html().
  */
 function mitds_preprocess_html(&$variables) {
-  global $user;
-  if (arg(0) == "node" && arg(2) == "invite") {
-    $node = node_load(arg(1));
-    $variables['classes_array'][] = "node-".$node->type;
-  }
-  if(user_is_logged_in()) {
-   $variables['classes_array'][] = _generate_subscription_class($user->uid);
-  }
+	global $user;
+	if (arg(0) == "node" && arg(2) == "invite") {
+		$node = node_load(arg(1));
+		$variables['classes_array'][] = "node-" . $node->type;
+	}
+	if (user_is_logged_in()) {
+		$variables['classes_array'][] = _generate_subscription_class($user->uid);
+	}
 }
-
 
 /**
  * Implements hook_menu_alter().
  */
 function mitds_menu_alter(&$items) {
-  $items['messages/view/%privatemsg_thread']['type'] = MENU_CALLBACK;
+	$items['messages/view/%privatemsg_thread']['type'] = MENU_CALLBACK;
 }
 
 /**
@@ -75,145 +69,136 @@ function mitds_menu_alter(&$items) {
  */
 function mitds_menu_link_alter(&$link) {
 
-  if ($link['link_title'] == 'messages/view/%privatemsg_thread') {
+	if ($link['link_title'] == 'messages/view/%privatemsg_thread') {
 
-    $link['options']['alter'] = TRUE;
-  }
+		$link['options']['alter'] = TRUE;
+	}
 }
 
 function mitds_form_comment_form_alter(&$form, &$form_state) {
 
-  $form['wrapper']['#type'] = 'fieldset';
-  $form['wrapper']['#title'] = 'Your Message';
-  $form['wrapper']['#collapsible'] = FALSE;
-  $form['wrapper']['author'] = $form['author'];
-  unset($form['author']);
+	$form['wrapper']['#type'] = 'fieldset';
+	$form['wrapper']['#title'] = 'Your Message';
+	$form['wrapper']['#collapsible'] = FALSE;
+	$form['wrapper']['author'] = $form['author'];
+	unset($form['author']);
 
-  //Comment
-  $form['wrapper']['comment_body'] = $form['comment_body'];
-  unset($form['comment_body']);
+	//Comment
+	$form['wrapper']['comment_body'] = $form['comment_body'];
+	unset($form['comment_body']);
 
-  $form['field_proposal_files']['#type'] = 'markup';
-  $form['wrapper']['field_proposal_files'] = $form['field_proposal_files'];
-  unset($form['field_proposal_files']);
+	$form['field_proposal_files']['#type'] = 'markup';
+	$form['wrapper']['field_proposal_files'] = $form['field_proposal_files'];
+	unset($form['field_proposal_files']);
 
+	$form['actions']['submit']['#value'] = 'Send Message';
+	$form['actions']['submit']['#attributes']['class'][] = 'btn-success mar-top10';
 
+	$form['wrapper']['actions'] = $form['actions'];
+	unset($form['actions']);
 
-  $form['actions']['submit']['#value'] = 'Send Message';
-  $form['actions']['submit']['#attributes']['class'][] = 'btn-success mar-top10';
-
-  $form['wrapper']['actions'] = $form['actions'];
-  unset($form['actions']);
-
-
-  $form['author']['homepage']['#access'] = FALSE;
-  $form['author']['mail']['#required'] = TRUE;
+	$form['author']['homepage']['#access'] = FALSE;
+	$form['author']['mail']['#required'] = TRUE;
 }
 
 function mitds_image_style($variables) {
-  if ($variables['style_name'] == 'blog_teaser' ) {
-     $variables['attributes'] = array(
-        'class' => 'thumb',
-      );
-  }
+	if ($variables['style_name'] == 'blog_teaser') {
+		$variables['attributes'] = array(
+			'class' => 'thumb',
+		);
+	}
 
-  $dimensions = array(
-    'width' => $variables['width'],
-    'height' => $variables['height'],
-  );
+	$dimensions = array(
+		'width' => $variables['width'],
+		'height' => $variables['height'],
+	);
 
-  image_style_transform_dimensions($variables['style_name'], $dimensions);
+	image_style_transform_dimensions($variables['style_name'], $dimensions);
 
-  $variables['width'] = $dimensions['width'];
-  $variables['height'] = $dimensions['height'];
+	$variables['width'] = $dimensions['width'];
+	$variables['height'] = $dimensions['height'];
 
+	// Determine the url for the styled image.
+	$variables['path'] = image_style_url($variables['style_name'], $variables['path']);
 
-  // Determine the url for the styled image.
-  $variables['path'] = image_style_url($variables['style_name'], $variables['path']);
-
-  return theme('image', $variables);
+	return theme('image', $variables);
 }
 
-
-function _mitds_access_invite(){
-  return TRUE;
-  $node = node_load(arg(1));
-  if($node->type == "opportunity"){
-    return TRUE;
-  }
-  return FALSE;
+function _mitds_access_invite() {
+	return TRUE;
+	$node = node_load(arg(1));
+	if ($node->type == "opportunity") {
+		return TRUE;
+	}
+	return FALSE;
 }
 
 function mitds_preprocess_comment(&$vars) {
 
-  $comment = $vars['comment'];
-  $uri = entity_uri('comment', $comment);
-  $uri['options'] += array('attributes' => array(
-    'class' => 'permalink',
-    'rel' => 'bookmark',
-  ));
-   $vars['permalink'] = l('#' . $vars['id'], $uri['path'], $uri['options']);
+	$comment = $vars['comment'];
+	$uri = entity_uri('comment', $comment);
+	$uri['options'] += array('attributes' => array(
+		'class' => 'permalink',
+		'rel' => 'bookmark',
+	));
+	$vars['permalink'] = l('#' . $vars['id'], $uri['path'], $uri['options']);
 }
 
 function mitds_menu_link(array $variables) {
-  $element = $variables['element'];
-  $sub_menu = '';
+	$element = $variables['element'];
+	$sub_menu = '';
 
-  if ($element['#below']) {
-    // Prevent dropdown functions from being added to management menu so it
-    // does not affect the navbar module.
-    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
-      $sub_menu = drupal_render($element['#below']);
-    }
-    elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
-      // Add our own wrapper.
-      unset($element['#below']['#theme_wrappers']);
-      $sub_menu = '<ul>' . drupal_render($element['#below']) . '</ul>';
-      // Generate as standard dropdown.
-      $element['#title'] .= ' <span class="caret"></span>';
-      $element['#attributes']['class'][] = 'dropdown';
-      $element['#localized_options']['html'] = TRUE;
+	if ($element['#below']) {
+		// Prevent dropdown functions from being added to management menu so it
+		// does not affect the navbar module.
+		if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+			$sub_menu = drupal_render($element['#below']);
+		} elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
+			// Add our own wrapper.
+			unset($element['#below']['#theme_wrappers']);
+			$sub_menu = '<ul>' . drupal_render($element['#below']) . '</ul>';
+			// Generate as standard dropdown.
+			$element['#title'] .= ' <span class="caret"></span>';
+			$element['#attributes']['class'][] = 'dropdown';
+			$element['#localized_options']['html'] = TRUE;
 
-      // Set dropdown trigger element to # to prevent inadvertant page loading
-      // when a submenu link is clicked.
-      $element['#localized_options']['attributes']['data-target'] = '#';
-      $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
-      $element['#localized_options']['attributes']['class'][] = 'sf-with-ul';
-      $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
-    }
+			// Set dropdown trigger element to # to prevent inadvertant page loading
+			// when a submenu link is clicked.
+			$element['#localized_options']['attributes']['data-target'] = '#';
+			$element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+			$element['#localized_options']['attributes']['class'][] = 'sf-with-ul';
+			$element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+		}
 
-  }
-  // On primary navigation menu, class 'active' is not set on active menu item.
-  // @see https://drupal.org/node/1896674
-  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
-    $element['#attributes']['class'][] = 'active';
-  }
+	}
+	// On primary navigation menu, class 'active' is not set on active menu item.
+	// @see https://drupal.org/node/1896674
+	if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+		$element['#attributes']['class'][] = 'active';
+	}
 
-  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
-  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+	$output = l($element['#title'], $element['#href'], $element['#localized_options']);
+	return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
 function mitds_menu_tree($variables) {
-  return '<ul class="sf-menu">' . $variables['tree'] . '</ul>';
+	return '<ul class="sf-menu">' . $variables['tree'] . '</ul>';
 }
 
 function mitds_menu_tree__menu_admin_blog_management($variables) {
-  return '<ul class="list-group">' . $variables['tree'] . '</ul>';
+	return '<ul class="list-group">' . $variables['tree'] . '</ul>';
 }
 
-function mitds_menu_link__menu_admin_blog_management($variables){
-  $element = $variables['element'];
-  $sub_menu = '';
+function mitds_menu_link__menu_admin_blog_management($variables) {
+	$element = $variables['element'];
+	$sub_menu = '';
 
-  if ($element['#below']) {
-    $sub_menu = drupal_render($element['#below']);
-  }
-  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
-  $element['#attributes']['class'][] = 'list-group-item';
-  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+	if ($element['#below']) {
+		$sub_menu = drupal_render($element['#below']);
+	}
+	$output = l($element['#title'], $element['#href'], $element['#localized_options']);
+	$element['#attributes']['class'][] = 'list-group-item';
+	return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
-
-
-
 
 ?>
