134b259f80c6f4efc6750dc76862e865b73cfdcb
Salu S Nair <salu.s@lightrains.com>
2017-12-05 14:52:44 +0530

    Error code

--- index.php
+++ index.php
@@ -19,3 +19,7 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
