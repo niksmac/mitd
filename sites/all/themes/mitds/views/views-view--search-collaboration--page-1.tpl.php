2548745eed68f59cf79861f55f1b4b9debdf19fa
Salu S Nair <salu.s@lightrains.com>
2017-12-06 11:21:14 +0530

    Collboration title font size changes

--- sites/all/themes/mitds/views/views-view--search-collaboration--page-1.tpl.php
+++ sites/all/themes/mitds/views/views-view--search-collaboration--page-1.tpl.php
@@ -22,7 +22,7 @@
 		<div class="listing-section">
 			<div class="listing-ver-3">
 				<div class="listing-heading">
-					<h4><?php echo $value['title']; ?></h4>
+					<h5><?php echo $value['title']; ?></h5>
 				</div>
 				<div class="listing-inner">
 					<div class="listing-content">
